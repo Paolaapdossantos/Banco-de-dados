54957d75b22282ace98ca1f056312fe4fdcff1e8
Paola <paolaapdossantos222@gmail.com>
2026-03-17 20:26:50 -0300

    ATUALIZANDO OS NOMES DOS ARQUIVOS E SEUS CODIGOS

---
+++ manutencao_procedure.sql
@@ -0,0 +1,42 @@
+---desenvolver uma procedure que receba um id de funcionario e um numero que represente o porcentual de aumento do
+-- salario do funcionario, para realizar o auamneto do id do funcionario Se existir, o
+--porcentual deve estar ebtre maior que 0 e menor que 10. retornarl 0 para indicar que deu certo o processo e
+--o salario do funcionario aumentou. se der erro, retornar -999,-999, etc (documente). O teto de salario 
+--max 10000,cehegou a 10000nao aumenta mais o salario 
+
+
+
+
+CREATE OR REPLACE
+PROCEDURE PROC_AUMENTAR_SALARIO(P_ID IN HR.PESSOA.ID%TYPE,
+                                P_PORCENT IN NUMBER,
+                                P_SAIDA OUT NUMBER) ----- TRES PARAMETROS, 3 DE ENTRADA 1 SAIDA-----------
+IS 
+    V_QTDE NUMBER;
+    V_SALARIO_ATUAL NUMBER;
+    V_SALARIO_NOVO NUMBER;
+BEGIN 
+                 IF P_PORCENT >0 AND P_PORCENT  <10  THEN    
+                        SELECT COUNT(*) INTO V_QTDE
+                        FROM PESSOA
+                        WHERE ID=P_ID AND SALARIO <10000;
+                        (V_SALARIO_ATUAL= SALARIO /)
+                        V_SALARIO_NOVO= (SALARIO*P_PORCENT/100 )
+                        IF CONDICAO AQUI 
+                      IF V_QTDE = 0 THEN 
+                        UPDATE 
+                        P_SAIDA :=0; --INSERIDO COM SUCESSO
+                 ELSE 
+                    P_SAIDA:=-999; --JÁ EXISTE NA TABELA
+                 END IF
+        
+
+   COMMIT;
+EXCEPTION  
+WHEN OTHERS THEN 
+    ROLLBACK;
+    P_SAIDA:=SQLCODE;
+    DBMS_OUTPUT.PUT_LINE('CODIGO DO ERRO  É: ' || SQLCODE);
+    DBMS_OUTPUT.PUT_LINE('DESCRICAO DO ERRO  É: ' || SQLERRM);
+    DBMS_OUTPUT.PUT_LINE('LINHA DO ERROÉ: ' || DBMS_UTILITY.FORMAT_ERROR_BACKTRACE);
+END;
